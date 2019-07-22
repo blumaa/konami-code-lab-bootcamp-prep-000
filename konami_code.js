584b48af81889cef9d0593852997c8805e7e9220
Aaron Blum <blumaa@gmail.com>
2019-07-22 19:35:21 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -16,23 +16,23 @@ document.body.addEventListener('ArrowDown', function(event) {
 });
 
 
-// let index = 0;
+let index = 0;
 
-// function onKeyDownHandler(e) {
-//   const key = e.key;
+function onKeyDownHandler(e) {
+   const key = e.key;
  
-//   if (key === alphabet[index]) {
-//     index++;
+   if (key === alphabet[index]) {
+     index++;
  
-//     if (index === alphabet.length) {
-//       alert("Hurray!");
+     if (index === alphabet.length) {
+       alert("Hurray!");
  
-//       index = 0;
-//     }
-//   } else {
-//     index = 0;
-//   }
-// }
+       index = 0;
+     }
+   } else {
+     index = 0;
+   }
+ }
 // function init() {
 //   // your code here
 // }
